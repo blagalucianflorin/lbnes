e66f11f6e98a7ca68f84020597862628c5954c68
Blaga Lucian-Florin <blaga.lucianflorin@gmail.com>
2022-09-03 19:57:22 +0300

    Clean main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-#include "include/nes.h"
-
+#include "nes.h"
 
 int main (int argc, char **argv)
 {
@@ -7,29 +6,6 @@ int main (int argc, char **argv)
 
     nes my_nes (rom_file);
     my_nes.start ();
-
-//    SMB example
-//
-//    my_nes.toggle_joypad (1);
-//
-//    for(int i = 0; i < 60; i++)
-//        my_nes.render_frame ();
-//
-//    for(int i = 0; i < 60; i++)
-//    {
-//        my_nes.set_button (joypad::START);
-//        my_nes.render_frame ();
-//    }
-//
-//    for(int i = 0; i < 60; i++)
-//        my_nes.render_frame ();
-//
-//
-//    for(int i = 0; i < 300; i++)
-//    {
-//        my_nes.set_button (joypad::DPAD_RIGHT);
-//        my_nes.render_frame ();
-//    }
 
     return (0);
 }
